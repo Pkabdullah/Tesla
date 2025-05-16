272fdeec32bac0c71427c4ac6cc43f8153679aa9
Muhammad Abdullah <124899260+Pkabdullah@users.noreply.github.com>
2025-05-16 12:38:11 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,6 +30,7 @@ const Footer = () => {
                 <div className="max-w-screen-xl mx-auto px-4 sm:px-10 lg:px-20 ">
                     <div className="flex justify-between items-center max-sm:flex-col max-sm:items-start">
 
+
                         <div className="max-w-[50%] max-sm:max-w-full">
                             <Image src={"/logo.png"} alt="logo" width={80} height={50} />
                             <p className="text-[15px] max-md:text-[12px] max-sm:text-[10px]">
@@ -37,50 +38,56 @@ const Footer = () => {
                             </p>
                         </div>
 
-                        <div className="flex flex-col mr-16 max-md:mr-0 max-md:ml-10 max-sm:mt-10">
-                            <div className="flex space-x-4 gap-6 mt-16 max-sm:gap-3 max-sm:space-x-0 max-sm:flex-col">
+                        <div className="flex gap-20 max-md:gap-12 max-sm:flex-col max-sm:gap-8 max-sm:w-full max-sm:mt-10">
 
-                                <ul className="flex flex-col space-y-2">
-                                    <h6 className="text-[20px] max-md:text-[15px] max-sm:text-[14px] mb-4 ">Categories</h6>
-                                    <Link href="#products">  <li className="text-[15px] max-md:text-[12px] max-sm:text-[11px] list-none  hover:text-green-400 transition-all duration-300 ease-in-out">3D Series</li></Link>
-                                    <Link href="#products"> <li className="text-[15px] max-md:text-[12px] max-sm:text-[11px]  list-none  hover:text-green-400 transition-all duration-300 ease-in-out">Atheletic Series</li></Link>
-                                    <Link href="#products"> <li className="text-[15px] max-md:text-[12px] max-sm:text-[11px]  list-none  hover:text-green-400 transition-all duration-300 ease-in-out">Cyberpunk Series</li></Link>
-                                    <Link href="#products"> <li className="text-[15px] max-md:text-[12px] max-sm:text-[11px]  list-none  hover:text-green-400 transition-all duration-300 ease-in-out">Misc Series</li></Link>
-                                    <Link href="#products"><li className="text-[15px] max-md:text-[12px] max-sm:text-[11px]  list-none  hover:text-green-400 transition-all duration-300 ease-in-out">Pastel Series</li></Link>
-                                    <Link href="#products"> <li className="text-[15px] max-md:text-[12px] max-sm:text-[11px]  list-none  hover:text-green-400 transition-all duration-300 ease-in-out">Retro Series</li></Link>
-                                </ul>
+                            <ul className="flex flex-col space-y-2 max-sm:space-y-1">
+                                <h6 className="text-[20px] max-md:text-[15px] max-sm:text-[14px] mb-4 max-sm:mb-2 text-left">Categories</h6>
+                                {["3D Series", "Athletic Series", "Cyberpunk Series", "Misc Series", "Pastel Series", "Retro Series"].map((item, index) => (
+                                    <Link href="#products" key={index}>
+                                        <li className="text-[15px] max-md:text-[12px] max-sm:text-[11px] hover:text-green-400 transition-all duration-300 ease-in-out list-none text-left">
+                                            {item}
+                                        </li>
+                                    </Link>
+                                ))}
+                            </ul>
 
-                                <ul className="flex flex-col space-y-2">
-                                    <h6 className="text-[20px] max-md:text-[15px] max-sm:text-[14px] mb-4">Legal</h6>
-                                    <Link href="">     <li className="text-[15px] max-md:text-[12px] max-sm:text-[11px] list-none  hover:text-green-400 transition-all duration-300 ease-in-out">Terms & Conditions</li></Link>
-                                    <Link href="">        <li className="text-[15px] max-md:text-[12px] max-sm:text-[11px] list-none  hover:text-green-400 transition-all duration-300 ease-in-out">Privacy Policy</li></Link>
+                            <ul className="flex flex-col space-y-2 max-sm:space-y-1">
+                                <h6 className="text-[20px] max-md:text-[15px] max-sm:text-[14px] mb-4 max-sm:mb-2 text-left">Legal</h6>
+                                <Link href="">
+                                    <li className="text-[15px] max-md:text-[12px] max-sm:text-[11px] hover:text-green-400 transition-all duration-300 ease-in-out list-none text-left">
+                                        Terms & Conditions
+                                    </li>
+                                </Link>
+                                <Link href="">
+                                    <li className="text-[15px] max-md:text-[12px] max-sm:text-[11px] hover:text-green-400 transition-all duration-300 ease-in-out list-none text-left">
+                                        Privacy Policy
+                                    </li>
+                                </Link>
 
-                                    <h6 className="text-[20px] mt-8 max-md:text-[15px] max-sm:text-[14px]">Follow Us</h6>
+                                <h6 className="text-[20px] mt-8 max-md:text-[15px] max-sm:text-[14px] text-left">Follow Us</h6>
+                                <div className="flex flex-row gap-4 text-white max-sm:justify-start">
+                                    {socialMedia.map((item, i) => (
+                                        <Link href="/" key={i}>
+                                            <div className="flex items-center justify-center w-10 h-10 bg-[#3af96e] text-black rounded-full transition-transform duration-300 transform hover:-translate-y-1">
+                                                <item.icon size={20} />
+                                            </div>
+                                        </Link>
+                                    ))}
+                                </div>
+                            </ul>
 
-                                    <div className="flex flex-row gap-4 text-white">
-                                        {socialMedia.map((item, i) => (
-                                            <Link href="/" key={i}>
-                                                <div className="flex items-center justify-center w-10 h-10 bg-[#3af96e] text-black rounded-full transition-transform duration-300 transform hover:-translate-y-1">
-                                                    <item.icon size={20} />
-                                                </div>
-                                            </Link>
-                                        ))}
-                                    </div>
 
-                                </ul>
-
-                            </div>
                         </div>
                     </div>
 
-                    <div className="text-center">
+                    <div className="text-left mt-8 max-sm:mt-6">
                         <hr className="border-1 border-[#3af96e] w-full mt-8 max-sm:mt-4" />
                     </div>
 
                     <div className="w-full text-center text-white">
 
 
-                        <h2 className="mt-4 flex justify-center items-center gap-1 text-[15px] max-sm:text-[13px]">
+                        <h2 className="mt-4 flex justify-center items-center max-sm:flex max-sm:justify-start  gap-1 text-[15px] max-sm:text-[13px]">
                             <FaRegCopyright /> 2025<Link href="/"> <span className="text-[#3af96e]">Tesla</span>.</Link> All rights reserved
                         </h2>
 
